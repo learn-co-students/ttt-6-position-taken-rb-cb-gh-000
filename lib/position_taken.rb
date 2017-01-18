0f28e0b95a41f007aaf41d74e66daeb5780c134d
Ahmet Teke <ateke1@binghamton.edu>
2017-01-18 19:06:01 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] != nil && index < board.length && index >= 0
+    return board[index].strip != "" 
+  else 
+    return false
+  end
+end

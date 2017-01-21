041999604fd35df7fd54d715356ea2dd64777e57
Peter McCrory <peter.mccrory@gmail.com>
2017-01-21 08:02:51 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board,user_spot)
+  #check_idx = user_spot - 1
+  if board[user_spot] == " " || board[user_spot] == "" || board[user_spot] == nil
+    return false
+  else
+    return true
+  end
+end

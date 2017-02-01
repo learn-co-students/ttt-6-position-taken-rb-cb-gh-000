6fe2b680c312771104ada2aa4e1234577682680c
Zachari LaCombe <zacharilouis@gmail.com>
2017-02-01 07:03:48 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  element = board[index]
+
+  if element.nil? || board[index].strip == ""
+    return false
+  end
+  
+  return true
+end

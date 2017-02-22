dd7524120df3bcbbb9ea29ff83c6c69b4cf30221
Ilian Herzi <ilianherzi@berkeley.edu>
2017-02-22 04:23:11 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+# assumes that the index is within the bounds of the board array
+def position_taken?(board, index)
+  if(board[index] == " " || board[index] == "" || board[index] == nil)
+    return false
+  else
+    return true
+  end
+end

37f5c51ec8237495d7b3ad4fd8c62ad11f66d5e2
Ethan Chen <chenh43@mcmaster.ca>
2017-03-13 04:58:19 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+def position_taken?(board, i)
+  if board[i] == " "
+    return false
+  elsif board[i] == ""
+    return false
+  elsif board[i] == nil
+    return false
+  else 
+    return true
+  end
+end

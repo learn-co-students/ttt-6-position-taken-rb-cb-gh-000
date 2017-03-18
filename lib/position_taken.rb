b9a7894148982e0677f706c8feefed4ba7c1f23a
Hiep Nguyen <tahi1990@gmail.com>
2017-03-18 15:04:19 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  index = index.to_i
+  if board[index] == nil || board[index].strip == ""
+    return false
+  else
+    return true
+  end
+end

70b5f5c15885ff381e11d0de6faf77abb2243829
Edmundo Elizondo <edmundo.geg@hotmail.com>
2017-05-18 03:14:10 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,5 @@
 # code your #position_taken? method here!
+def position_taken? (board_array, index)
+  is_free = board_array[index] == " " || board_array[index] == "" || board_array[index] == nil
+  !is_free
+end

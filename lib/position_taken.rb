39cf782f039ed9149c224de09edcbd36a5cfbede
Praveen Thirumurugan <praveen@pixlmatrix.com>
2017-06-26 07:09:24 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken?(board, pos)
+  p = pos.to_i
+  if board[p] == "X" || board[p] == "O"
+    return true
+  end
+  if board[p] == " " || board[p] == "" || board[p] == nil
+    return false
+  end
+
+end

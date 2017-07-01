b1e7f596170075adf87fbf219b0506bf47aa1757
Shekh Ataul <ataul.one@gmail.com>
2017-07-01 06:09:01 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,22 @@
 # code your #position_taken? method here!
+
+board = [" "," "," "," "," "," "," "," "," "]
+def position_taken?(array,index)
+  if array[index] == nil
+    return false
+  else
+    if array[index] == ""
+      return false
+    else
+      if array[index] == " "
+        return false
+      else
+        if array[index] == "X" || array[index] == "O"
+            return true
+        end
+
+      end
+
+    end
+  end
+end

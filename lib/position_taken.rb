fbe9fed52a0ad7ef0bf6de41888bb30ffcf87525
Esther Liao <erliao@princeton.edu>
2017-07-08 22:50:44 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken? (board, index)
+  if board == []
+  # elsif board[index] == nil
+  else
+    !(board[index] == "" || board[index] == " " || board[index] == nil)
+  end
+end

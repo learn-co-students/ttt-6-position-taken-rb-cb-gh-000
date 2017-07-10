cae36241a9af5c8bdeb0ca98894b4f1980a70a56
Lashaunna Samuels <shaunna191@hotmail.com>
2017-07-10 14:41:29 -0400

    Condensed my method for consciousness and readability

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-    if board[index] == "" 
-        return false
-    elsif board[index] == " "
-        return false
-    elsif board[index] == nil
-        return false
-    elsif board[index] == "X" || "O"
-        return true
-    else return true
-    end
+   board[index] == "" || board[index] == " "
+   board[index] == "X" || board[index] == "O"
 end

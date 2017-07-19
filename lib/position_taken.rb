850512fd1d1d91640d5c24974ebfabd2feff51c7
Manuel Zdovc <manuel.zdovc@web.de>
2017-07-19 02:00:56 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == nil
-    return false
-  end
-  board[index].strip != ""
+  !!board[index] && board[index].strip != ""
 end

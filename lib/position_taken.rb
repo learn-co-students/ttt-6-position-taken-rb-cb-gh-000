d7363efca7eba94317fbae6131fed621171dc7f3
Jan Lukas Eckel <j1n.l21k1s.e3k5l@gmail.com>
2017-08-05 18:47:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, numb)
+  if (board[numb] == " " || board[numb] == "" || board[numb] == nil)
+    false
+  else
+    true
+  end
+end

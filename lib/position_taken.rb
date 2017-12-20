8ea9ceef123ef7ad3aa8cab8050d1505642d8db2
abdelrauof mohammed <abdelrauof97@gmail.com>
2017-12-20 04:21:21 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+
+board = ["X", "X", "", "", "", "", "", "", ""]
+
+def position_taken?(board, index)
+  if "#{board[index]}" == nil || "#{board[index]}" == "" || "#{board[index]}" == " "
+    return false
+  else
+    return true
+  end
+  raise "#{board[position]} is not a valid move"
+end
+position_taken?(board, 1)
+puts position_taken?(board, 1)

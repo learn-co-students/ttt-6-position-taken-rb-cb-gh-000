f57828b093a0e7460a31ce4bc5b269c9d7df4ad6
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-23 03:12:21 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  isHere = false
+  if board[index] == " " || board[index] == ""
+    isHere
+  elsif board[index] == nil
+    isHere
+  else
+    !isHere
+  end
+end

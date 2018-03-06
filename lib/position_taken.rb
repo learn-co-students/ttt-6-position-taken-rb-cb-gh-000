fc358ddd3d0aa7dd9e2090a7e11421d6574e1974
Nor Kagramanian <nor8111@gmail.com>
2018-03-06 18:59:56 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(array, index)
+  return false if array[index] == " " || array[index] == "" || array[index] == nil
+  return true if array[index] == "X" || array[index] == "O"
+
+
+
+end

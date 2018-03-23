b1797d511a4423c25b3a525892c503b0ccd34b6b
Harkirat Singh <harkirat.hira@gmail.com>
2018-03-23 20:05:04 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(fir, sec)
+  if (fir[sec] == " ") || (fir[sec] == "") || (fir[sec] == nil)
+    return false
+  else
+    return true
+  end
+end

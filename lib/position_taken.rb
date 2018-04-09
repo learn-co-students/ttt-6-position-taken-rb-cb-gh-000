dc188a99c2ea3c00275727896d1e0e80df0e0992
Xavier Codinas Bañeres <xavier19966@gmail.com>
2018-04-09 12:13:26 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(array, position)
+  if array[position] == " " || array[position] == "" || array[position] == nil
+    return false
+  elsif array[position] == "X" || array[position] == "O"
+    return true
+  end
+end

49b8fd83259ae0e30885d3c9a17bca83b2e2c55f
Kirill Akhmetzyanov <kirill94a@mail.ru>
2018-07-18 14:58:14 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  b=true
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    b=false
+  end
+  return b
+end

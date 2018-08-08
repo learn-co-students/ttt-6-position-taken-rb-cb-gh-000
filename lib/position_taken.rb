601169c93c25d23f37c433f5429dcad34613ba63
Sergiu Marton <alexander.sergiu@gmail.com>
2018-08-08 07:50:27 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+    cell = board[index]
+    if cell == " " || cell == "" || cell == nil
+        false
+    else
+        true
+    end
+end

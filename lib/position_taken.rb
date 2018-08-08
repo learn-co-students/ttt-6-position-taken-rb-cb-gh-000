f6b2773b647152e819a4c367443caf42d7919092
Shane Glass <shaneryanglass88@gmail.com>
2018-08-08 22:20:09 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  square = board[index]
+  if square == nil
+    FALSE
+  elsif square.strip == ""
+    FALSE
+  else
+    TRUE
+  end
+end

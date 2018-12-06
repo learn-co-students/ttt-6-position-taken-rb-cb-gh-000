8f902f177de9a7695424567aa9127f4d1a8e7d84
khalid Lamsadi <khalid.lamsadi@outlook.fr>
2018-12-06 12:52:36 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(array, index)
+  if (array[index] == " ") || (array[index] == "")
+    return false
+  elsif array[index] == nil
+    return false
+  elsif array[index] == "X" || "O"
+    return true
+  end
+end

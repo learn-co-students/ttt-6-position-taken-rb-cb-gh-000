7a2b0e0f8c7dcec41b2033bb5e1db2c9decd5b47
YUNFEI HU <yunfeih1@student.unimelb.edu.au>
2018-12-27 07:48:33 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
-# code your #position_taken? method here!
+# code your #position_taken? method here.
+def position_taken?(board, index)
+  if board[index.to_i] == "X" or board[index.to_i] == "O"
+    return true
+  else
+    return false
+  end
+end

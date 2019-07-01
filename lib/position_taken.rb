cac646a7653f871a06c924d7e2321d3763efebbc
Venkatesh Mangnale <mangnalev.1999@gmail.com>
2019-07-01 03:49:43 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board,index)
+  entry = board[index]
+  if entry == ''||entry = ' '||entry == nil
+    false
+  else
+    true
+  end
+end

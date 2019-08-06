6f1e1d4f44a087d37043fdcf806e5644e4f897e4
Juan Diego Gomez Peña <juandi.gomezp@utadeo.edu.co>
2019-08-06 03:05:59 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,6 +4,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
  def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     return true
-  else return false
+  else
+    return false
   end
 end

70dd10de401fd03c7bc11d30916f6f11e742ad24
Ben Francisco <bpfranci@math.tamu.edu>
2019-08-18 21:26:52 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+#position_taken? Check to see if a position is taken in a tic-tac-toe board
+# =>  board - the current board state.
+# =>  index - the index of the board to checking
+# <=  false - if position is not taken
+# <=  true - if the position is taken
+def position_taken?(board, index)
+  return !( board[index] == " " ||
+            board[index] == "" ||
+            board[index] == nil)
+end

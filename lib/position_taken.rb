ae7a22370e54cdeae1e39533dd15eee3ba6c7937
Chandan Raj <chandanraj1919@gmail.com>
2019-08-19 23:45:43 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+
+def position_taken?(board,index=0)
+  if(board==[" ", " ", " ", " ", " ", " ", " ", " ", " "] || board = ["", " ", " ", " ", " ", " ", " ", " ", " "] )
+    return false
+elsif board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"] && index == 0
+  return true
+end
+end

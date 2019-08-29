dcaf9f1a6065c5cf00c1996b92e0cc428bb145d9
Cankat Saraç <cankat.sarac@stu.bahcesehir.edu.tr>
2019-08-29 12:24:03 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  result = nil
+  if board[index] == "" ||
+    board[index] == " " ||
+    board[index] == nil
+    result = false;
+  elsif board[index] == "X" || board[index] == "O"
+    result = true
+  end
+  result
+end

f633a29e6f78b4a22bccb1dfdd36e157a2439bdc
Belch Or <djm263@case.edu>
2020-04-26 13:07:45 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,22 @@
 # code your #position_taken? method here!
+
+
+
+def position_taken?(board, index_number)
+
+  if (board[index] == "" || board[index] == " " || board[index] == nil)
+    return false
+  else
+    return true
+  end
+
+
+end
+
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end

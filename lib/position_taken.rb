6087553eef3116b0a3f4310cf103f598fceaf918
Murat Dönmez <muratcandonmez123@gmail.com>
2020-05-11 09:20:42 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
-# code your #position_taken? method here!
+# code your #position_taken? method
+
+board = ["", "", " ", "X", " ", " ", " ", " ", " "]
+
+
+def position_taken?(board, index)
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+puts position_taken?(board, 0)
+puts position_taken?(board, 1)
+puts position_taken?(board, 2)
+puts position_taken?(board, 3)
+puts position_taken?(board, 4)

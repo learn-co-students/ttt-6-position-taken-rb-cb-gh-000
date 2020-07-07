a862076b62765366f027e32bc63872ebbcb48f32
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-07 09:09:56 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
 # code your #position_taken? method here!
+def position_taken?(arr=[" "," "," "," "," "," "," "," "," "],index=1)
+  if (arr[index]=="X")||(arr[index]=="O")
+    return true
+  end
+  return false
+end
